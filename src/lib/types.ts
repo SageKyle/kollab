f4ec44732a28a1da61195bc820ee5e9fd70c92d6
SageKyle <ominyipaulo@gmail.com>
2023-12-10 13:00:11 +0100

    added initial structure of profile page

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,9 @@
+// import { IconType } from 'react-icons'
+
 export interface userType {
 	firstName: string
 	lastName: string
 	username: string
 	email: string
+	avatar?: string
 }
--- src/pages/dashboard/profile.tsx
+++ src/pages/dashboard/profile.tsx
@@ -1,14 +1,34 @@
+import { RiAddCircleFill } from 'react-icons/ri'
+import profileAvatar from '../../assets/icons/user.svg'
+import { userType } from '../../lib/types'
+
+const sampleUser: userType = {
+	firstName: 'John',
+	lastName: 'Doe',
+	email: 'johndoe@email.com',
+	username: 'johnny',
+	avatar: profileAvatar,
+}
+
 export default function Profile() {
 	return (
-		<div>
-			<h1>This is Profile page</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quod
-				obcaecati cupiditate, repellendus culpa labore blanditiis! Porro
-				ratione, minus cumque, ut et consectetur ipsum facilis totam harum,
-				maxime mollitia officiis dolorem adipisci assumenda! Unde sit amet
-				veniam minus, nam illo.
-			</p>
-		</div>
+		<section
+			aria-description="profile page"
+			className="w-full p-8 mt-8 mr-10 rounded bg-white shadow"
+		>
+			<div className="w-full flex items-center justify-start gap-8 md:gap-16">
+				<div className="text-4xl border-4 border-black rounded-full p-1 object-center object-fill w-24 aspect-square text-primary flex relative">
+					<img src={sampleUser.avatar} alt="avatar" />
+					<RiAddCircleFill className="inline-block text-2xl z-10 bg-white rounded-full absolute bottom-4 -right-2 cursor-pointer w-6" />
+				</div>
+				<div className="flex flex-col justify-start items-start gap-0">
+					<h4 className="font-semibold text-primary text-xl">{`${sampleUser.firstName} ${sampleUser.lastName}`}</h4>
+					<small>@{sampleUser.username}</small>
+					<p className="mt-2 font-mont max-w-full text-ellipsis whitespace-nowrap font-extralight">
+						{sampleUser.email}
+					</p>
+				</div>
+			</div>
+		</section>
 	)
 }
