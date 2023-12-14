502def331a82d01fcbea342dd9960f935815e5d3
SageKyle <ominyipaulo@gmail.com>
2023-12-14 19:01:07 +0100

    install/initialize firebase

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyAHZ0vIzWawDaAL2C04-WQcs4MKkkXOCGk',
+	authDomain: 'tenbits-1.firebaseapp.com',
+	projectId: 'tenbits-1',
+	storageBucket: 'tenbits-1.appspot.com',
+	messagingSenderId: '415286767465',
+	appId: '1:415286767465:web:2f2b5bcc3f9d0d63d8d7f8',
+	measurementId: 'G-XYPXNL0JGP',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
+export const auth = getAuth(app)
